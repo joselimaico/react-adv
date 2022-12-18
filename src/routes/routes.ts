95035cd25c1e1eef2f07874a8271c4898ca49796
limaico <francisco.limaico@kushkipagos.com>
2022-12-18 15:48:36 -0500

    LazyLoad basico completado

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,8 @@
-//  import { LazyPage1, LazyPage2, LazyPage3 } from "../01-lazyload/pages";
-
 import { lazy, LazyExoticComponent } from "react";
 
-const lazy1 = lazy(() => import("../01-lazyload/pages/LazyPage1"));
-const lazy2 = lazy(() => import("../01-lazyload/pages/LazyPage2"));
-const lazy3 = lazy(() => import("../01-lazyload/pages/LazyPage3"));
+const lazy1 = lazy(() => import(/* webpackChunkName: LazyPage1 */"../01-lazyload/pages/LazyPage1"));
+const lazy2 = lazy(() => import(/* webpackChunkName: LazyPage2 */"../01-lazyload/pages/LazyPage2"));
+const lazy3 = lazy(() => import(/* webpackChunkName: LazyPage3 */"../01-lazyload/pages/LazyPage3"));
 
 type JSXComponent = () => JSX.Element;
 
